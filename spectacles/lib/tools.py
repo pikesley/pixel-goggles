5d02e9f3ff40a7c1bdd0501fc06e0f9f14710129
pikesley <sam.pikesley@demandlogic.co.uk>
2024-12-05 22:02:10 +0000

    Extract the ugly rotational index calculations

--- spectacles/lib/tools.py
+++ spectacles/lib/tools.py
@@ -14,3 +14,13 @@ def _first_index(index, offset, length):
 def _second_index(index, offset, length):
     """Get the second half of a pair."""
     return (offset - length - index - 1) % length
+
+
+def clockwise_index(index, offset, length):
+    """Get an index for rotating clockwise."""
+    return (index + offset) % length
+
+
+def anti_clockwise_index(index, offset, length):
+    """Get an index for rotating anti-clockwise."""
+    return length - 1 - ((index + offset) % length)
--- spectacles/tests/test_tools.py
+++ spectacles/tests/test_tools.py
@@ -1,4 +1,4 @@
-from lib.tools import index_pairs
+from lib.tools import anti_clockwise_index, clockwise_index, index_pairs
 
 
 def test_index_pairs():
@@ -20,3 +20,19 @@ def test_index_pairs():
         (8, 11),
         (9, 10),
     ]
+
+
+def test_clockwise_index():
+    """Test clockwise-index."""
+    assert clockwise_index(0, 0, 12) == 0
+    assert clockwise_index(1, 3, 12) == 4  # noqa: PLR2004
+    assert clockwise_index(10, 3, 12) == 1
+
+
+def test_anti_clockwise_index():
+    """Test anti-clockwise index."""
+    assert anti_clockwise_index(0, 0, 12) == 11  # noqa: PLR2004
+    assert anti_clockwise_index(1, 0, 12) == 10  # noqa: PLR2004
+    assert anti_clockwise_index(2, 0, 12) == 9  # noqa: PLR2004
+
+    assert anti_clockwise_index(5, 8, 12) == 10  # noqa: PLR2004
