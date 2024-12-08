e5f85fb4bfc07a13ccbfec7b903f13a263c0f0b2
pikesley <sam.pikesley@demandlogic.co.uk>
2024-12-08 22:36:46 +0000

    Fully testable orderings

--- spectacles/lib/orderings.py
+++ spectacles/lib/orderings.py
@@ -2,13 +2,30 @@ ring_size = 16
 tops = {"right": 10, "left": 22}
 
 
-def get_ordering(side, start, direction):  # noqa: ARG001
+def get_prime(side, start):
+    """Work out the prime position."""
+    offsets = {
+        "north": 0,
+        "west": 1,
+        "south": 2,
+        "east": 3,
+    }
+
+    prime = (tops[side] + offsets[start] * (ring_size / 4)) % ring_size
+
+    if side == "left":
+        prime += ring_size
+
+    return int(prime)
+
+
+def get_ordering(side, start, direction):
     """Calculate an ordering."""
     offset = 0
     if side == "left":
-        offset = 16
+        offset = ring_size
 
-    prime = tops[side]
+    prime = get_prime(side, start)
     ordering = [prime]
     tail = list(range(prime - 1, -1 + offset, -1)) + list(
         range(ring_size - 1 + offset, prime, -1)
@@ -18,17 +35,3 @@ def get_ordering(side, start, direction):  # noqa: ARG001
         tail.reverse()
 
     return ordering + tail
-
-
-# orderings = {
-#     "left-hand": {
-#         "clockwise": clockwise_left_hand,
-#         "anticlockwise": list(reversed(clockwise_left_hand[1:]))
-#         + [clockwise_left_hand[0]],
-#     },
-#     "right-hand": {
-#         "clockwise": clockwise_right_hand,
-#         "anticlockwise": list(reversed(clockwise_right_hand[1:]))
-#         + [clockwise_right_hand[0]],
-#     },
-# }
--- spectacles/tests/test_orderings.py
+++ spectacles/tests/test_orderings.py
@@ -1,4 +1,4 @@
-from lib.orderings import get_ordering
+from lib.orderings import get_ordering, get_prime
 
 
 def test_get_ordering():
@@ -39,40 +39,90 @@ def test_get_ordering():
         8,
         9,
     ]
+    assert get_ordering("left", "north", "clockwise") == [
+        22,
+        21,
+        20,
+        19,
+        18,
+        17,
+        16,
+        31,
+        30,
+        29,
+        28,
+        27,
+        26,
+        25,
+        24,
+        23,
+    ]
+    assert get_ordering("left", "north", "clockwise") == [
+        22,
+        21,
+        20,
+        19,
+        18,
+        17,
+        16,
+        31,
+        30,
+        29,
+        28,
+        27,
+        26,
+        25,
+        24,
+        23,
+    ]
+    assert get_ordering("right", "east", "clockwise") == [
+        6,
+        5,
+        4,
+        3,
+        2,
+        1,
+        0,
+        15,
+        14,
+        13,
+        12,
+        11,
+        10,
+        9,
+        8,
+        7,
+    ]
+    assert get_ordering("left", "south", "clockwise") == [
+        30,
+        29,
+        28,
+        27,
+        26,
+        25,
+        24,
+        23,
+        22,
+        21,
+        20,
+        19,
+        18,
+        17,
+        16,
+        31,
+    ]
 
-    assert get_ordering("left", "north", "clockwise") == [
-        22,
-        21,
-        20,
-        19,
-        18,
-        17,
-        16,
-        31,
-        30,
-        29,
-        28,
-        27,
-        26,
-        25,
-        24,
-        23,
-    ]
-    assert get_ordering("left", "north", "clockwise") == [
-        22,
-        21,
-        20,
-        19,
-        18,
-        17,
-        16,
-        31,
-        30,
-        29,
-        28,
-        27,
-        26,
-        25,
-        24,
-        23,
-    ]
+
+def test_get_prime():
+    """Test we can find the prime spot."""
+    assert get_prime("right", "north") == 10  # noqa: PLR2004
+    assert get_prime("left", "north") == 22  # noqa: PLR2004
+
+    assert get_prime("right", "west") == 14  # noqa: PLR2004
+    assert get_prime("left", "west") == 26  # noqa: PLR2004
+
+    assert get_prime("right", "south") == 2  # noqa: PLR2004
+    assert get_prime("left", "south") == 30  # noqa: PLR2004
+
+    assert get_prime("right", "east") == 6  # noqa: PLR2004
+    assert get_prime("left", "east") == 18  # noqa: PLR2004
