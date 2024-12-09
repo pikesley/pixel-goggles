98370b1409e85b92b5348d90b85ab5688ee0a419
pikesley <sam.pikesley@demandlogic.co.uk>
2024-12-09 15:48:17 +0000

    Make testable locally

--- spectacles/lib/context.py
+++ spectacles/lib/context.py
@@ -1,10 +1,15 @@
-import machine
-import neopixel
+import os
+
+if os.environ["TEST"] == 1:
+    import machine
+    import neopixel
 
 from conf import leds, pin, seconds_per_rotation
 from lib.time_based_hue_source import TimeBasedHueSource
 
-pixels = neopixel.NeoPixel(machine.Pin(pin), leds)
-length = len(pixels)
+if os.environ["TEST"] == 1:
+    pixels = neopixel.NeoPixel(machine.Pin(pin), leds)
+    length = len(pixels)
+
 brightness = 1
 hue_source = TimeBasedHueSource(seconds_per_rotation)
