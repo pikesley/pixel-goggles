29d68aeffa0e643cde85b5afa263c175da676e99
pikesley <sam.pikesley@demandlogic.co.uk>
2024-12-09 15:30:00 +0000

    Mediocre sparkle

--- spectacles/patterns/patterns.py
+++ spectacles/patterns/patterns.py
@@ -1,11 +1,13 @@
 from patterns.rainbow import rainbow
 from patterns.rainbow_wave import rainbow_wave
+from patterns.sparkle import sparkle
 from patterns.spinner import spinner
 from patterns.wave import wave
 
 patterns = [
+    sparkle,
+    spinner,
     rainbow_wave,
     wave,
     rainbow,
-    spinner,
 ]
--- spectacles/patterns/sparkle.py
+++ spectacles/patterns/sparkle.py
@@ -1,31 +1,19 @@
 import time
-from random import randint
+from random import random
 
 from lib.colour_tools import rgb_from_hue, scale_colour
-from lib.context import brightness, hue_source, length, pixels
+from lib.context import hue_source, length, pixels
 
 
 def sparkle():
     """Randomly light up."""
-    sleep_time = 80
-    max_actives = 10
-    active_leds = []
+    sleep_time = 1000
 
     while True:
         colour = rgb_from_hue(hue_source.hue())["bytes"]
 
-        candidate = randint(0, length - 1)  # noqa: S311
-        while candidate not in active_leds:
-            candidate = randint(0, length - 1)  # noqa: S311
-            active_leds.append(candidate)
-
-        if len(active_leds) > max_actives:
-            active_leds = active_leds[1:]
-
-        for index, led_index in enumerate(active_leds):
-            pixels[led_index] = scale_colour(
-                colour, ((length - index) / length) * brightness
-            )
+        for index in range(length):
+            pixels[index] = scale_colour(colour, random())  # noqa: S311
 
         pixels.write()
         time.sleep_ms(sleep_time)
