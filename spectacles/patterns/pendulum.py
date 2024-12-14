05c9292310d7a219e8ec889b9d9ca899c1f54959
pikesley <sam.pikesley@demandlogic.co.uk>
2024-12-14 16:35:59 +0000

    Removed redundancy

--- spectacles/patterns/pendulum.py
+++ spectacles/patterns/pendulum.py
@@ -5,70 +5,71 @@ from lib.context import pixels
 from lib.orderings import get_ordering
 from lib.tools import inverse_square_tail, off, pendulum_timings
 
+offset = 2
+timing_scale = 70
+
+tail_length = 8
+tail_off_factor = 2
+
+left = get_ordering("left", "n", "clockwise", overlap=True)[offset:-offset]
+right = get_ordering("right", "n", "clockwise", overlap=True)[offset:-offset]
+timings = pendulum_timings()[offset - 1 : -offset + 1]
+
+tail = inverse_square_tail(tail_length)
+
 
 def pendulum():
     """Spin the wheel."""
-    offset = 2
-    timing_scale = 70
-
-    tail_length = 8
-    tail_off_factor = 2
-
-    left = get_ordering("left", "n", "clockwise", overlap=True)[offset:-offset]
-    right = get_ordering("right", "n", "clockwise", overlap=True)[offset:-offset]
-    timings = pendulum_timings()[offset - 1 : -offset + 1]
-
-    tail = inverse_square_tail(tail_length)
-
     while True:
-        tail_count = 0
-        time.sleep_ms(timing_scale)
-        for i in range(len(left)):
-            off(pixels)
-            colour = just_an_rgb()
+        roll()
+        roll(direction="anti-clockwise")
 
-            if tail_count < tail_length:
-                tail_count += 1
+def roll(direction="clockwise"):
+    """Roll around."""
+    full_range = range(len(left))
+    if "anti" in direction:
+        full_range = range(len(left) - 1, -1, -1)
 
-            for t in range(tail_count):
-                tail_index = i - t
-                for eye in [left, right]:
-                    pixels[eye[tail_index]] = scale_colour(colour, tail[t])
+    tail_count = 0
 
-            pixels.write()
-            interval = int(timings[i] * timing_scale)
-            time.sleep_ms(interval)
+    for i in full_range:
+        off(pixels)
 
-        while tail_count > 1:
-            for eye in [left, right]:
-                victim_index = eye[0 - (tail_count)]
-                pixels[victim_index] = (0, 0, 0)
-            pixels.write()
-            tail_count -= 1
-            time.sleep_ms(int(timing_scale / tail_off_factor))
+        if tail_count < tail_length:
+            tail_count += 1
 
-        tail_count = 0
-        time.sleep_ms(timing_scale)
-        for i in range(len(left) - 1, -1, -1):
-            off(pixels)
-            colour = just_an_rgb()
+        paint_tail(i, tail_count, direction)
 
-            if tail_count < tail_length:
-                tail_count += 1
+        interval = int(timings[i] * timing_scale)
+        time.sleep_ms(interval)
 
-            for t in range(tail_count):
-                tail_index = i + t
-                for eye in [left, right]:
-                    pixels[eye[tail_index]] = scale_colour(colour, tail[t])
+    erase_tail(direction=direction)
 
-            pixels.write()
-            interval = int(timings[i] * timing_scale)
-            time.sleep_ms(interval)
+def paint_tail(index, count, direction):
+    """Paint the tail."""
+    colour = just_an_rgb()
 
-        while tail_count > 1:
-            for eye in [left, right]:
+    for t in range(count):
+        tail_index = index - t
+        if "anti" in direction:
+            tail_index = index + t
+
+        for eye in [left, right]:
+            pixels[eye[tail_index]] = scale_colour(colour, tail[t])
+
+    pixels.write()
+
+def erase_tail(direction):
+    """Clean up the tail."""
+    tail_count = tail_length
+    while tail_count > 2:
+        for eye in [left, right]:
+
+            victim_index = eye[0 - (tail_count)]
+            if "anti" in direction:
                 victim_index = eye[tail_count - 1]
-                pixels[victim_index] = (0, 0, 0)
-            pixels.write()
-            tail_count -= 1
-            time.sleep_ms(int(timing_scale / tail_off_factor))
+
+            pixels[victim_index] = (0, 0, 0)
+        pixels.write()
+        tail_count -= 1
+        time.sleep_ms(int(timing_scale / tail_off_factor))
