f68895a6518f6293806e5b0f157feab233e5fb5f
pikesley <sam.pikesley@demandlogic.co.uk>
2024-12-23 22:10:42 +0000

    Holy shit it works
    OK let's find out how to draw text

--- screen/main.py
+++ screen/main.py
@@ -1,46 +1,42 @@
 from machine import Pin, SoftI2C
 
 i2c = SoftI2C(sda=Pin(9), scl=Pin(8), freq=400000)
-
 device = 0x3E
-b = bytearray(1)
 
-# turn screen on
-b[0] = 255
-i2c.writeto_mem(device, 0x22, b)
+size = {
+    "x": 240,
+    "y": 135
+}
 
-b[0] = 1
-i2c.writeto_mem(device, 0x20, b)
+def initialise():
+    """Clear screen."""
+    b = bytearray(1)
 
-# rotate screen
-b[0] = 6
-i2c.writeto_mem(device, 0x36, b)
+    # turn screen on
+    b[0] = 255
+    i2c.writeto_mem(device, 0x22, b)
 
-command = 0x6a
+    # invert colours
+    b[0] = 1
+    i2c.writeto_mem(device, 0x20, b)
 
-x_left = 0x00
-y_top = 0x00
-x_right = 0x44
-y_bottom = 0x10
+    # rotate screen
+    b[0] = 1
+    i2c.writeto_mem(device, 0x36, b)
 
-colour = 0x4325
+    fill_screen((255, 255, 255))
+
+def fill_screen(colour):
+    """Fill the screen with `(r, g, b)`."""
+    command = 0x6B
+    i2c.writeto_mem(
+        device,
+        command,
+        bytearray(
+            [0, 0, size["x"], size["y"], colour[0], colour[1], colour[2]]
+        )
+    )
 
 
-commands = [command, x_left, y_top, x_right, y_bottom, colour]
-i2c.writeto_mem(device, command, bytearray(commands))
-
-# i2c.writeto_mem(device, 0x62, bytearray([0x20, 0x20, 0xf8, 0x00]) )
-# i2c.writeto_mem(device, 0x62, bytearray([0x21, 0x20, 0xf8, 0x00]) )
-# i2c.writeto_mem(device, 0x62, bytearray([0x22, 0x20, 0xf8, 0x00]) )
-# i2c.writeto_mem(device, 0x62, bytearray([0x23, 0x20, 0xf8, 0x00]) )
-# i2c.writeto_mem(device, 0x62, bytearray([0x25, 0x20, 0xf8, 0x00]) )
-# i2c.writeto_mem(device, 0x62, bytearray([0x27, 0x20, 0xf8, 0x00]) )
-# i2c.writeto_mem(device, 0x62, bytearray([0x29, 0x20, 0xf8, 0x00]) )
-# i2c.writeto_mem(device, 0x62, bytearray([0x31, 0x20, 0xf8, 0x00]) )
-# i2c.writeto_mem(device, 0x62, bytearray([0x33, 0x20, 0xf8, 0x00]) )
-# i2c.writeto_mem(device, 0x62, bytearray([0x35, 0x20, 0xf8, 0x00]) )
-# i2c.writeto_mem(device, 0x62, bytearray([0x37, 0x20, 0xf8, 0x00]) )
-# i2c.writeto_mem(device, 0x62, bytearray([0x39, 0x20, 0xf8, 0x00]) )
-# i2c.writeto_mem(device, 0x62, bytearray([0x41, 0x20, 0xf8, 0x00]) )
-# i2c.writeto_mem(device, 0x62, bytearray([0x43, 0x20, 0xf8, 0x00]) )
-# i2c.writeto_mem(device, 0x62, bytearray([0x45, 0x20, 0xf8, 0x00]) )
+initialise()
+fill_screen((255, 0, 0))
