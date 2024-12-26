c8c665323227849567433b2b7422da9d04b97979
pikesley <sam.pikesley@demandlogic.co.uk>
2024-12-26 22:45:10 +0000

    RLE gets odd at big scales

--- screen/lib/screen.py
+++ screen/lib/screen.py
@@ -96,12 +96,8 @@ class ST7789v2:
         self.send_command(0x2A, x_offsets)
         self.send_command(0x2B, y_offsets)
 
-        data = text_data(
-            text,
-            scale_factor=scale_factor,
-            on_colour=colour,
-        )
-
+        data = text_data(text, scale_factor=scale_factor, on_colour=colour, rle=False)
+        # 0x49 for RLE
         self.send_command(0x41, data)
 
 
