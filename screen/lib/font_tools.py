fb83e090bbf64e79a893e43ff2a96f13a609079f
pikesley <sam.pikesley@demandlogic.co.uk>
2024-12-26 12:11:31 +0000

    Test colouring

--- screen/lib/font_tools.py
+++ screen/lib/font_tools.py
@@ -1,6 +1,6 @@
-def bytes_to_bits(bytes):
+def bytes_to_bits(byte_list):
     """Turn bytes into lists of bits."""
-    return [list(f"{i:#010b}"[2:]) for i in bytes]
+    return [[int(i) for i in list(f"{byte:#010b}"[2:])] for byte in byte_list]
 
 
 def scale_bits(bits, scale):
@@ -9,10 +9,31 @@ def scale_bits(bits, scale):
     for line in bits:
         scaled_bits.append([])
         for bit in line:
-            for i in range(scale):
+            for _ in range(scale):
                 scaled_bits[-1].append(bit)
 
-        for j in range(scale - 1):
+        for _ in range(scale - 1):
             scaled_bits.append(scaled_bits[-1])
 
     return scaled_bits
+
+
+def colour_bits(bits, on_colour, off_colour):
+    """Apply colours to bits."""
+    if isinstance(on_colour, int):
+        on_colour = (on_colour,)
+    if isinstance(off_colour, int):
+        off_colour = (off_colour,)
+
+    coloured_bits = []
+    for line in bits:
+        coloured_bits.append([])
+        for bit in line:
+            if bit == 0:
+                for element in off_colour:
+                    coloured_bits[-1].append(element)
+            if bit == 1:
+                for element in on_colour:
+                    coloured_bits[-1].append(element)
+
+    return coloured_bits
