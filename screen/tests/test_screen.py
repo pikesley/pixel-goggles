00c8d91955389669073cf92e9e8e1c506662d90a
pikesley <sam.pikesley@demandlogic.co.uk>
2024-12-27 22:38:24 +0000

    Bytes as ints
    Much more readable

--- screen/tests/test_screen.py
+++ screen/tests/test_screen.py
@@ -9,10 +9,10 @@ def test_initialisation():
     _ = ST7789v2(i2c=mocked_i2c)
 
     assert mocked_i2c.mock_calls == [
-        call.writeto_mem(62, 34, bytearray(b"\xff")),
-        call.writeto_mem(62, 32, bytearray(b"\x00")),
-        call.writeto_mem(62, 54, bytearray(b"\x01")),
-        call.writeto_mem(62, 105, bytearray(b"\x00\x00\xf0\x87\x00")),
+        call.writeto_mem(62, 34, bytearray([255])),
+        call.writeto_mem(62, 32, bytearray([0])),
+        call.writeto_mem(62, 54, bytearray([1])),
+        call.writeto_mem(62, 105, bytearray([0, 0, 240, 135, 0])),
     ]
 
 
@@ -22,7 +22,7 @@ def test_different_starting_background():
     _ = ST7789v2(i2c=mocked_i2c, background_colour=(255, 0, 0))
 
     assert mocked_i2c.mock_calls[-1] == call.writeto_mem(
-        62, 105, bytearray(b"\x00\x00\xf0\x87\xe0")
+        62, 105, bytearray([0, 0, 240, 135, 224])
     )
 
 
@@ -33,7 +33,7 @@ def test_clearing_screen():
     st.clear_screen()
 
     assert mocked_i2c.mock_calls[-1] == call.writeto_mem(
-        62, 105, bytearray(b"\x00\x00\xf0\x87\xe0")
+        62, 105, bytearray([0, 0, 240, 135, 224])
     )
 
 
@@ -44,12 +44,106 @@ def test_clearing_screen_with_a_different_colour():
     st.clear_screen()
 
     assert mocked_i2c.mock_calls[-1] == call.writeto_mem(
-        62, 105, bytearray(b"\x00\x00\xf0\x87\xe0")
+        62, 105, bytearray([0, 0, 240, 135, 224])
     )
 
     st.clear_screen((0, 255, 0))
     assert mocked_i2c.mock_calls[-1] == call.writeto_mem(
-        62, 105, bytearray(b"\x00\x00\xf0\x87\x1c")
+        62, 105, bytearray([0, 0, 240, 135, 28])
     )
 
     assert st.background_colour == (0, 255, 0)
+
+
+def test_writing_text():
+    """Test we can write text."""
+    mocked_i2c = MagicMock()
+    st = ST7789v2(i2c=mocked_i2c, background_colour=(255, 0, 0))
+
+    st.write_text("A", x=0, y=0, colour=(0, 255, 0), scale_factor=1)
+
+    assert mocked_i2c.mock_calls[-3] == call.writeto_mem(62, 42, bytearray([0, 7]))
+    assert mocked_i2c.mock_calls[-2] == call.writeto_mem(62, 43, bytearray([0, 7]))
+    assert mocked_i2c.mock_calls[-1] == call.writeto_mem(
+        62,
+        65,
+        bytearray(
+            [
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                28,
+                28,
+                28,
+                28,
+                0,
+                0,
+                0,
+                28,
+                0,
+                0,
+                0,
+                0,
+                28,
+                0,
+                0,
+                28,
+                0,
+                0,
+                0,
+                0,
+                28,
+                0,
+                0,
+                28,
+                28,
+                28,
+                28,
+                28,
+                28,
+                0,
+                0,
+                28,
+                0,
+                0,
+                0,
+                0,
+                28,
+                0,
+                0,
+                28,
+                0,
+                0,
+                0,
+                0,
+                28,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+            ]
+        ),
+    )
+
+
+def test_writing_centered_text():
+    """Test we can write centered text."""
+    mocked_i2c = MagicMock()
+    st = ST7789v2(i2c=mocked_i2c, background_colour=(255, 0, 0))
+
+    st.write_text("A", x="centered", y="centered", colour=(0, 0, 255), scale_factor=4)
+
+    assert mocked_i2c.mock_calls[-3] == call.writeto_mem(62, 42, bytearray([104, 135]))
+    assert mocked_i2c.mock_calls[-2] == call.writeto_mem(62, 43, bytearray([51, 82]))
