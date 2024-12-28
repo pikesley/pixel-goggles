9452ee349f378952dcd7f833d37e14477978125d
pikesley <sam.pikesley@demandlogic.co.uk>
2024-12-28 16:44:59 +0000

    Write "now" and "next"

--- goggles/patterns_list.py
+++ goggles/patterns_list.py
@@ -12,9 +12,9 @@ from patterns.tilting_rainbow import tilting_rainbow
 from patterns.wave import wave
 
 patterns = [
+    tilting_rainbow,
     rainbow,
     tilter,
-    tilting_rainbow,
     sparkle,
     spots,
     responder,
--- goggles/runner.py
+++ goggles/runner.py
@@ -3,7 +3,7 @@ import time
 from lib.context import on_board
 from lib.pattern_index_manager import manage_index
 from patterns_list import patterns
-from screen.lib.screen import screen
+from screen.screen import screen, size
 
 
 def run():
@@ -16,7 +16,7 @@ def run():
     except IndexError:
         pattern = patterns[0]
 
-    update_screen(pattern)
+    update_screen(index)
     pattern()
 
 
@@ -27,11 +27,28 @@ def blink_onboard(duration=100):
         time.sleep_ms(duration)
 
 
-def update_screen(pattern):
+def update_screen(index):
     """Update the screen."""
+    now_pattern = patterns[index].__name__
+    next_pattern = patterns[(index + 1) % len(patterns)].__name__
+
     screen.write_text(
-        pattern.__name__.replace("_", " "),
+        title_case(now_pattern)[:14],
         x="centered",
         y="centered",
-        colour=(255, 255, 255),
+        colour=(255, 0, 255),
+        scale_factor=2,
     )
+
+    screen.write_text(
+        title_case(next_pattern),
+        x="centered",
+        y=size["y"] - 24,
+        colour=(0, 0, 255),
+        scale_factor=2,
+    )
+
+
+def title_case(name):
+    """Fix up name."""
+    return " ".join([s[0].upper() + s[1:].lower() for s in name.split("_")])
