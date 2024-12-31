00bab3b2c0b7cff619d220d15de24c523a0362f7
pikesley <sam.pikesley@demandlogic.co.uk>
2024-12-31 13:35:35 +0000

    Remove screen

--- goggles/main.py
+++ goggles/main.py
@@ -1,5 +1,6 @@
 import asyncio
 
+
 from lib.bluetooth_controller import (
     await_connection,
     ble_service,
@@ -7,7 +8,6 @@ from lib.bluetooth_controller import (
 )
 from lib.pattern_index_manager import manage_index
 from patterns_list import patterns
-from screen.screen import screen, size
 from vendor.aioble import aioble
 
 
@@ -20,49 +20,7 @@ def get_pattern():
     except IndexError:
         pattern = patterns[0]
 
-    update_screen(index)
     return pattern
-
-
-def update_screen(index):
-    """Update the screen."""
-    big_scale = 2
-    small_scale = 1
-
-    big_colour = (255, 0, 255)
-    small_colour = (0, 0, 255)
-
-    y_offset = 48
-
-    now_pattern = patterns[index].__name__
-    next_pattern = patterns[(index + 1) % len(patterns)].__name__
-
-    if "_" in now_pattern:
-        now_pattern = now_pattern.split("_")
-        y_offset = y_offset - (big_scale * 4)
-    else:
-        now_pattern = [now_pattern]
-
-    try:
-        for i, line in enumerate(now_pattern):
-            y_val = y_offset + (i * 8 * big_scale)
-            screen.write_text(
-                title_case(line),
-                x="centered",
-                y=y_val,
-                colour=big_colour,
-                scale_factor=big_scale,
-            )
-
-        screen.write_text(
-            title_case(next_pattern),
-            x="centered",
-            y=size["y"] - 32,
-            colour=small_colour,
-            scale_factor=small_scale,
-        )
-    except MemoryError:
-        pass
 
 
 def title_case(name):
