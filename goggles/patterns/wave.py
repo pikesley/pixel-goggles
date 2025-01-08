26e79795144cec2462fecbd78bebe6db499e5574
pikesley <sam.pikesley@demandlogic.co.uk>
2025-01-08 18:47:47 +0000

    Both ways

--- goggles/patterns/wave.py
+++ goggles/patterns/wave.py
@@ -1,4 +1,5 @@
 import asyncio
+import gc
 
 from lib.colour_tools import just_an_rgb, scale_colour
 from lib.context import goggles, pixels
@@ -7,21 +8,29 @@ from lib.tools import get_intervals, inverse_square_tail
 
 async def wave():
     """Waves of colour."""
-    sleep_multiplier = 150
-    values = inverse_square_tail(27, coefficient=0.5, backwards=True)
+    sleep_multiplier = 50
+    length = 36
+    values = inverse_square_tail(length, coefficient=0.5, backwards=True)
     intervals = get_intervals(sleep_multiplier)
 
-    goggles.load_ordering(point="w", rotation="pairs")
+    directions = {
+        "w": (goggles.left, goggles.right),
+        "e": (goggles.right, goggles.left),
+    }
 
     while True:
-        colour = just_an_rgb()
-        colours = [scale_colour(colour, v) for v in values]
+        for direction, sides in directions.items():
+            goggles.load_ordering(point=direction, rotation="pairs")
+            for _ in range(len(values)):
+                colour = just_an_rgb()
+                colours = [scale_colour(colour, v) for v in values]
 
-        goggles.left.fill(colours)
-        goggles.right.fill(colours[9:])
+                for index, offset in enumerate([0, 9]):
+                    sides[index].fill(colours[offset:])
 
-        pixels.write()
+                pixels.write()
 
-        await asyncio.sleep_ms(int(intervals.head))
-        intervals.rotate()
-        values.rotate(direction="r")
+                await asyncio.sleep_ms(int(intervals.head))
+                intervals.rotate()
+                values.rotate(direction="r")
+                gc.collect()
